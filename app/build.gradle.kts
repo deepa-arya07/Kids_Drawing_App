e7d7a3ad76006055e29a01f2f3160d61be7d2cc5
deepa_arya07 <deepa.arya1540@gmail.com>
2024-06-03 11:08:05 +0530

    updated

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -57,8 +57,13 @@ dependencies {
     implementation(platform("androidx.compose:compose-bom:2023.08.00"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
+
+    implementation ("androidx.appcompat:appcompat:1.5.1" )// Use the latest version
+    implementation( "androidx.core:core-ktx:1.9.0" )// For Kotlin extensions
+
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
